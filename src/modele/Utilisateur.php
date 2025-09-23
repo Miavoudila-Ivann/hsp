9a887b762e9ab983dac1929bbc00b8a1c8e25454
Ivann MIAVOUDILA <MIAVOUDILA_Iv@lprs.local>
2025-09-23 11:38:35 +0200

    fix(): ajout des prog du modele utilisateur et utilisateur repository

--- src/modele/Utilisateur.php
+++ src/modele/Utilisateur.php
@@ -1,8 +1,129 @@
 <?php
-
-namespace modele;
-
 class Utilisateur
 {
+    private $nom_uti;
+    private $idUtilisateur;
+    private $prenom_uti;
+    private $mail_uti;
+    private $mdp;
+    private $role;
+
+    /**
+     * @return mixed
+     */
+    public function getNomUti()
+    {
+        return $this->nom_uti;
+    }
+
+    /**
+     * @param mixed $nom_uti
+     */
+    public function setNomUti($nom_uti)
+    {
+        $this->nom_uti = $nom_uti;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenomUti()
+    {
+        return $this->prenom_uti;
+    }
+
+    /**
+     * @param mixed $prenom_uti
+     */
+    public function setPrenomUti($prenom_uti)
+    {
+        $this->prenom_uti = $prenom_uti;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdUtilisateur()
+    {
+        return $this->idUtilisateur;
+    }
+
+    /**
+     * @param mixed $idUtilisateur
+     */
+    public function setIdUtilisateur($idUtilisateur)
+    {
+        $this->idUtilisateur = $idUtilisateur;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMailUti()
+    {
+        return $this->mail_uti;
+    }
+
+    /**
+     * @param mixed $mail_uti
+     */
+    public function setMailUti($mail_uti)
+    {
+        $this->mail_uti = $mail_uti;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMdp()
+    {
+        return $this->mdp;
+    }
+
+    /**
+     * @param mixed $mdp
+     */
+    public function setMdp($mdp)
+    {
+        $this->mdp = $mdp;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param mixed $role
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+
+
+
+
+    public function __construct(array $donnees)
+    {
+        $this->hydrate($donnees);
+    }
+    public function hydrate($donnees)
+    {
+        foreach ($donnees as $key => $value)
+        {
+            $method = 'set'.ucfirst($key);
+
+            if (is_callable(array($this, $method)))
+            {
+                $this->$method($value);
+            }
+        }
+    }
+
 
 }
+?>
--- src/repository/UtilisateurRepository.php
+++ src/repository/UtilisateurRepository.php
@@ -1,8 +1,78 @@
+
 <?php
-
-namespace repository;
-
 class UtilisateurRepository
 {
+    private $bdd;
+
+    public function __construct()
+    {
+        $this->bdd = new Bdd();
+    }
+
+    public function inscription(Utilisateur $utilisateur)
+    {
+        $hashedPassword = password_hash($utilisateur->getMdp(), PASSWORD_DEFAULT);
+
+        $req = $this->bdd->getBdd()->prepare('INSERT INTO utilisateur (prenom_uti, nom_uti, mail_uti, mdp) VALUES (:prenom_uti, :nom_uti, :mail_uti, :mdp)');
+        $success = $req->execute([
+            "nom_uti" => $utilisateur->getNom(),
+            "prenom_uti" => $utilisateur->getPrenom(),
+            "email_uti" => $utilisateur->getEmail(),
+            "mdp" => $hashedPassword
+        ]);
+
+        return $success;
+    }
+
+    public function connexion($mail_uti, $mdp)
+    {
+        $req = $this->bdd->getBdd()->prepare('SELECT * FROM utilisateur WHERE mail_uti = :mail_uti');
+        $req->execute(['mail' => $mail_uti]);
+
+        $utilisateur = $req->fetch(PDO::FETCH_ASSOC);
+        var_dump($utilisateur);
+        var_dump(password_verify($mdp, $utilisateur['mdp']));
+        if ($utilisateur && password_verify($mdp, $utilisateur['mdp'])) {
+            return $utilisateur;
+        }
+
+        return false;
+    }
+
+    public function getUtilisateur($email) {
+        $query = "SELECT * FROM utilisateur WHERE mail_uti = :mail_uti";
+        $stmt = $this->bdd->getBdd()->prepare($query);
+        $stmt->bindValue(':mail_uti', $mail_uti);
+        $stmt->execute();
+        $userData = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($userData) {
+            return new Utilisateur($userData);
+        }
+        return null;
+    }
+    public function modifierUtilisateur(Utilisateur $utilisateur)
+    {
+
+        $req = $this->bdd->getBdd()->prepare('UPDATE utilisateur 
+            SET prenom_uti = :prenom_uti, nom_uti = :nom, $mail_uti = :mail_uti, role = :role
+            WHERE id_utilisateur = :id_utilisateur');
+        return $req->execute([
+            "id_utilisateur" => $utilisateur->getIdUtilisateur(),
+            "nom_uti" => $utilisateur->getNom(),
+            "prenom_uti" => $utilisateur->getPrenom(),
+            "mail_uti" => $utilisateur->getEmail(),
+            "role" => $utilisateur->getRole()
+        ]);
+    }
+    public function supprimerUtilisateur(Utilisateur $utilisateur){
+
+        $req = $this->bdd->getBdd()->prepare('DELETE FROM utilisateur WHERE id_utilisateur = :id_utilisateur');
+
+        return $req->execute([
+            "id_utilisateur" => $utilisateur->getIdUtilisateur(),
+        ]);
+    }
 
 }
+?>
