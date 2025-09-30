83c7cd6e707f29e23a4b453cae3be0f45eb4452b
Issa TOUZANINE <TOUZANINE_Is@lprs.local>
2025-09-30 09:51:05 +0200

    add : ajout de modele hopital

--- src/modele/Hopital.php
+++ src/modele/Hopital.php
@@ -4,5 +4,79 @@ namespace modele;
 
 class Hopital
 {
+    private $id_hopital;
+    private $nom;
+    private $adresse_hopital;
+    private $ville_hopital;
 
+    public function __construct(array $data = [])
+    {
+        if (isset($data['id_hopital'])) $this->id_hopital = $data['id_hopital'];
+        if (isset($data['nom'])) $this->nom = $data['nom'];
+        if (isset($data['adresse_hopital'])) $this->adresse_hopital = $data['adresse_hopital'];
+        if (isset($data['ville_hopital'])) $this->ville_hopital = $data['ville_hopital'];
+    }
+    /**
+     * @return mixed
+     */
+    public function getIdHopital()
+    {
+        return $this->id_hopital;
+    }
+
+    /**
+     * @param mixed $id_hopital
+     */
+    public function setIdHopital($id_hopital)
+    {
+        $this->id_hopital = $id_hopital;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdresseHopital()
+    {
+        return $this->adresse_hopital;
+    }
+
+    /**
+     * @param mixed $adresse_hopital
+     */
+    public function setAdresseHopital($adresse_hopital)
+    {
+        $this->adresse_hopital = $adresse_hopital;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVilleHopital()
+    {
+        return $this->ville_hopital;
+    }
+
+    /**
+     * @param mixed $ville_hopital
+     */
+    public function setVilleHopital($ville_hopital)
+    {
+        $this->ville_hopital = $ville_hopital;
+    }
 }
