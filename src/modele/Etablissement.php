0d771ef020a1f3ccf8e21c3f48006d3da71c5ce7
Issa TOUZANINE <TOUZANINE_Is@lprs.local>
2025-10-14 10:19:40 +0200

    edit : correction orthographe etablissement

--- src/modele/Etablissement.php
+++ src/modele/Etablissement.php
@@ -5,79 +5,57 @@ namespace modele;
 class Etablissement
 {
     private $id_etablissement;
+    private $nom_etablissement;
+    private $adresse_etablissement;
+    private $site_web_etablissement;
 
-    /**
-     * @return mixed
-     */
+    // Constructeur avec tableau associatif
+    public function __construct(array $data = [])
+    {
+        if (isset($data['id_etablissement'])) $this->id_etablissement = $data['id_etablissement'];
+        if (isset($data['nom_etablissement'])) $this->nom_etablissement = $data['nom_etablissement'];
+        if (isset($data['adresse_etablissement'])) $this->adresse_etablissement = $data['adresse_etablissement'];
+        if (isset($data['site_web_etablissement'])) $this->site_web_etablissement = $data['site_web_etablissement'];
+    }
+
+    // Getters & Setters
     public function getIdEtablissement()
     {
         return $this->id_etablissement;
     }
 
-    /**
-     * @param mixed $id_etablissement
-     */
     public function setIdEtablissement($id_etablissement)
     {
         $this->id_etablissement = $id_etablissement;
     }
 
-    /**
-     * @return mixed
-     */
-    public function getNomEtablissemnet()
+    public function getNomEtablissement()
     {
-        return $this->nom_etablissemnet;
+        return $this->nom_etablissement;
     }
 
-    /**
-     * @param mixed $nom_etablissemnet
-     */
-    public function setNomEtablissemnet($nom_etablissemnet)
+    public function setNomEtablissement($nom_etablissement)
     {
-        $this->nom_etablissemnet = $nom_etablissemnet;
+        $this->nom_etablissement = $nom_etablissement;
     }
 
-    /**
-     * @return mixed
-     */
     public function getAdresseEtablissement()
     {
         return $this->adresse_etablissement;
     }
 
-    /**
-     * @param mixed $adresse_etablissement
-     */
     public function setAdresseEtablissement($adresse_etablissement)
     {
         $this->adresse_etablissement = $adresse_etablissement;
     }
 
-    /**
-     * @return mixed
-     */
     public function getSiteWebEtablissement()
     {
         return $this->site_web_etablissement;
     }
 
-    /**
-     * @param mixed $site_web_etablissement
-     */
     public function setSiteWebEtablissement($site_web_etablissement)
     {
         $this->site_web_etablissement = $site_web_etablissement;
-    }
-    private $nom_etablissemnet;
-    private $adresse_etablissement;
-    private $site_web_etablissement;
-
-    public function __construct(array $data = [])
-    {
-        if (isset($data['id_etablissement'])) $this->id_etablissement = $data['id_etablissement'];
-        if (isset($data['nom_etablissement'])) $this->nom_etablissemnet = $data['nom_etablissement'];
-        if (isset($data['adresse_etablissement'])) $this->adresse_etablissement = $data['adresse_etablissement'];
-        if (isset($data['site_web_etablissement'])) $this->site_web_etablissement = $data['site_web_etablissement'];
     }
 }
