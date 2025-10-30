3afaa27625220c062da2a44787db5d9ea66f6be5
unknown <issatouzanine@gmail.com>
2025-10-30 11:36:30 +0100

    add : ajout des politique de confidentialiter

---
+++ src/vue/politique_confidentialite.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Politique de confidentialité</title>
+    <style>
+        body {
+            font-family: 'Segoe UI', sans-serif;
+            margin: 50px auto;
+            width: 80%;
+            line-height: 1.6;
+        }
+    </style>
+</head>
+<body>
+<h1>Politique de confidentialité</h1>
+<p>
+    Vos données sont collectées uniquement dans le cadre de la création et de la gestion de votre compte utilisateur.
+    Elles ne sont ni revendues, ni transmises à des tiers.
+</p>
+<p>
+    Conformément au Règlement Général sur la Protection des Données (RGPD),
+    vous pouvez demander à consulter, modifier ou supprimer vos informations personnelles
+    en nous contactant à l’adresse suivante : <strong>contact@hsp.fr</strong>.<br><br>
+    <a href="inscription.php">S'incrire</a>
+</p>
+</body>
+</html>
